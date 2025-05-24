df2e53a476827eb8753a6f7c2d934036d68f4c6c
Shubhachand Patel <118814330+Shubhachand@users.noreply.github.com>
2025-05-25 00:47:28 +0530

    Time: 19 ms (36.97%) | Memory: 15.8 MB (74.48%) - LeetSync

---
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string sortVowels(string s) {
+        vector<char> ans;
+        char x;
+        for (int i = 0; i < s.size(); i++) {
+            x = s[i];
+            if (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u' ||
+                x == 'A' || x == 'E' || x == 'I' || x == 'O' || x == 'U') {
+                    // if vowel then check capital or small and then sort them in order;
+                    ans.push_back(s[i]);
+
+            }
+
+
+
+        }
+        sort(ans.begin(),ans.end());
+        int j = 0;
+        for(int i = 0; i< s.size();i++){
+                x = s[i];
+            if (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u' ||
+                x == 'A' || x == 'E' || x == 'I' || x == 'O' || x == 'U') {
+                    // if vowel then check capital or small and then sort them in order;
+                    s[i] = ans[j++];
+
+            }
+        }
+       return s;
+    }
+};
