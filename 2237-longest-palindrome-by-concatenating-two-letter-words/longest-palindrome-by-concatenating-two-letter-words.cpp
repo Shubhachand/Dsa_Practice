b962cad2042b8de5a006e6ce1923d2cfd297be8f
Shubhachand Patel <118814330+Shubhachand@users.noreply.github.com>
2025-05-26 00:12:44 +0530

    Time: 86 ms (55.14%) | Memory: 172 MB (44.24%) - LeetSync

--- 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
-        unordered_map<string,int>map;
-    int count =0;
-        for(int i = 0 ; i < words.size();i++){
+        unordered_map<string, int> map;
+        int count = 0;
+        for (int i = 0; i < words.size(); i++) {
             string rev = words[i];
-            reverse(rev.begin(),rev.end());
-            if(map[rev]>0){
-                count+=4;
+            reverse(rev.begin(), rev.end());
+            if (map[rev] > 0) {
+                count += 4;
                 map[rev]--;
-            }else{
-              map[words[i]]++;
+            } else {
+                map[words[i]]++;
             }
         }
-        for(auto &it : map){
+        for (auto& it : map) {
             string word = it.first;
             int counts = it.second;
-            if(word[0]==word[1] && counts>0){
-                count+=2;
+            if (word[0] == word[1] && counts > 0) {
+                count += 2;
                 break;
             }
         }
