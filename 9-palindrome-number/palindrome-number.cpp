dc53a490f0620ed3fabf485d80281fc601e0c450
Shubhachand Patel <118814330+Shubhachand@users.noreply.github.com>
2025-07-04 19:28:19 +0530

    Time: 3 ms (46.67%) | Memory: 8.6 MB (68.34%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -3,8 +3,7 @@ public:
     bool isPalindrome(int num) {
         int rev = 0;
         int x = num;
-        if (x < 0)
-            return false;
+           if (x < 0 || (x % 10 == 0 && x != 0)) return false;
 
         while (x != 0) {
             int digit = x % 10;
