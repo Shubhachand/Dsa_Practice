af52a150c22adffb55236c96c12f3a18898471aa
Shubhachand Patel <118814330+Shubhachand@users.noreply.github.com>
2025-07-04 19:24:08 +0530

    Time: 3 ms (46.67%) | Memory: 8.6 MB (68.34%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -3,16 +3,20 @@ public:
     bool isPalindrome(int num) {
         int rev = 0;
         int x = num;
-        if(x<0) return false;
+        if (x < 0)
+            return false;
 
-        while(x!=0){
-            int digit = x%10;
-            if(rev>INT_MAX/10 || rev == INT_MAX/10 && digit >7 ) return false;
-        rev= (rev* 10) + digit;
-            
-        x/=10;
+        while (x != 0) {
+            int digit = x % 10;
+            if (rev > INT_MAX / 10 || rev == INT_MAX / 10 && digit > 7)
+                return false;
+            rev = (rev * 10) + digit;
+
+            x /= 10;
         }
-        if(num!=rev) return false;
-        else return true;
+        if (num != rev)
+            return false;
+        else
+            return true;
     }
 };
