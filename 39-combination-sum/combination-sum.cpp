0e9658783e8665c524f5c018edaee8cd05342f22
Shubhachand Patel <118814330+Shubhachand@users.noreply.github.com>
2025-08-01 03:23:39 +0530

    Time: 45 ms (17.02%) | Memory: 46.3 MB (13.08%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    void sum(int i, int target , vector<int>arr , vector<int>& temp, vector<vector<int>>&ans){
-        if(target == 0){
-             ans.push_back(temp);
-            
+    void sum(int i, int target, vector<int> arr, vector<int>& temp,
+             vector<vector<int>>& ans) {
+        if (target == 0) {
+            ans.push_back(temp);
+
             return;
         }
-        if(i==arr.size())return;
+        if (i == arr.size())
+            return;
 
-        if(arr[i]<=target){
-            temp.push_back(arr[i]);// store the element in ans vector
-            sum(i,target-arr[i],arr,temp,ans);
+        if (arr[i] <= target) {
+            temp.push_back(arr[i]); // store the element in ans vector
+            sum(i, target - arr[i], arr, temp, ans);
             temp.pop_back();
         }
-        sum(i+1,target,arr,temp,ans);
+        sum(i + 1, target, arr, temp, ans);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         int index = 0;
-       vector<vector<int>>ans;
-       vector<int>temp;
-        sum(index,target,candidates,temp,ans);
+        vector<vector<int>> ans;
+        vector<int> temp;
+        sum(index, target, candidates, temp, ans);
         return ans;
     }
 };
