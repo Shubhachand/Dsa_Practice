3fe1c8cb960207bb1d5869e283ecaf6936ea12e6
Shubhachand Patel <118814330+Shubhachand@users.noreply.github.com>
2025-08-04 19:36:02 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (85.12%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-void removeDupes(int i , vector<int>& nums,vector<int>& temp,set<vector<int>>&ans){
-    if(i == nums.size()){
-        ans.insert(temp);
-        return;
-    }
-    temp.push_back(nums[i]);
+void removeDupes(int idx , vector<int>& nums,vector<int>& temp,vector<vector<int>>&ans){
+    
+     ans.push_back(temp);
+    for(int i = idx ; i < nums.size();i++){
+        if(i>idx && nums[i] == nums[i-1]) continue;
+        temp.push_back(nums[i]);
     removeDupes(i+1,nums,temp,ans);
     temp.pop_back();
-    removeDupes(i+1,nums,temp,ans);
+    // removeDupes(i+1,nums,temp,ans);
+    }
 
 }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
           sort(nums.begin(), nums.end());
         vector<int>temp;
-        set<vector<int>> ans;
+        vector<vector<int>> ans;
         removeDupes(0,nums,temp,ans);
-        return vector<vector<int>>(ans.begin(),ans.end());
+        return ans;
     }
 };
