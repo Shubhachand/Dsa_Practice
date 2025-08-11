38eddb825ea624076b8b14e97084b00b2e043238
Shubhachand Patel <118814330+Shubhachand@users.noreply.github.com>
2025-08-11 18:21:20 +0530

    Time: 492 ms (17.05%) | Memory: 8.9 MB (26.65%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
-
-        bool isValid(int row , int col ,vector<vector<char>>& board ,char c){
-            for(int i = 0 ; i<board[0].size();i++){
-                if(board[i][col] == c) return false;
-                if(board[row][i] == c) return false;
-                if(board[3 * (row/3)  + (i/3) ] [3 * (col/3) + (i%3)] == c ) return false;
-            }
-            return true;
+    bool isValid(int row, int col, vector<vector<char>>& board, char c) {
+        for (int i = 0; i < board[0].size(); i++) {
+            if (board[i][col] == c)
+                return false;
+            if (board[row][i] == c)
+                return false;
+            if (board[3 * (row / 3) + (i / 3)][3 * (col / 3) + (i % 3)] == c)
+                return false;
         }
-        bool solve(vector<vector<char>>& board){
-            // base cond
+        return true;
+    }
+    bool solve(vector<vector<char>>& board) {
+        // base cond
 
-            for(int i = 0 ; i <board.size();i++){
-                    for(int j = 0 ;j<board[0].size();j++){
-                        if(board[i][j]=='.'){
-                            for(char c = '1'; c <= '9';c++){
-                                if(isValid(i,j,board,c)){
-                                    board[i][j] = c;
-                                    if(solve(board) == true) return true;
-                                    else board[i][j] = '.';
-                                }
-                            }
-                            return false;
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(i, j, board, c)) {
+                            board[i][j] = c;
+                            if (solve(board) == true)
+                                return true;
+                            else
+                                board[i][j] = '.';
                         }
                     }
+                    return false;
+                }
             }
-            return true;
         }
-    
-    void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
+        return true;
     }
+
+    void solveSudoku(vector<vector<char>>& board) { solve(board); }
 };
