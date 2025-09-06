3b782d5578b55acabe58105f26c4115a0abf27b0
Shubhachand Patel <118814330+Shubhachand@users.noreply.github.com>
2025-09-07 00:18:08 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (7.36%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -5,37 +5,38 @@ public:
 
         // for(int i = 0 ; i < nums.size();i++){
         //     mp[nums[i]]++;
-     
+
         // }
-       
+
         // int ans = 0;
         //   for (auto it = mp.begin(); it != mp.end(); it++) {
         //     if (it->second > nums.size()/2) {
-                
+
         //         ans = it->first;
         //     }
         // }
 
         // return ans;
         int c = 0;
-        int el ;
+        int el;
         int n = nums.size();
-        for(int i= 0;i<n;i++){
-            if(c == 0){
+        for (int i = 0; i < n; i++) {
+            if (c == 0) {
                 c = 1;
                 el = nums[i];
-            }else if (nums[i] != el){
+            } else if (nums[i] != el) {
                 c--;
-            }else{
+            } else {
                 c++;
             }
         }
         int c1 = 0;
-        for(int i = 0 ; i < n ; i++){
-            if(nums[i] == el) c1++;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == el)
+                c1++;
         }
 
-        if(c1>n/2){
+        if (c1 > n / 2) {
             return el;
         }
         return -1;
