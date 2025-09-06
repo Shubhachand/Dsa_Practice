a4b16287c0197324b8e574a2fd89b080d8b6e50d
Shubhachand Patel <118814330+Shubhachand@users.noreply.github.com>
2025-09-07 01:02:45 +0530

    Time: 0 ms (100.00%) | Memory: 71.6 MB (80.58%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -4,10 +4,10 @@ public:
         int sum = 0;
         int maxi = INT_MIN;
         int n = nums.size();
-        for(int i = 0 ; i < n ; i++){
-            sum+=nums[i];
-            maxi = max(sum,maxi);
-            if(sum < 0){
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            maxi = max(sum, maxi);
+            if (sum < 0) {
                 sum = 0;
             }
         }
