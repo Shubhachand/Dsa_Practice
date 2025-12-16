e14119973523b40dde85934a034e1386f3e99194
Shubhachand Patel <118814330+Shubhachand@users.noreply.github.com>
2025-12-16 15:24:49 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (59.03%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        for(int i = 0;i<nums.length;i++){
+            sum+=nums[i];
+        }
+        int ts = 0;
+        for(int i = 1;i<=nums.length;i++){
+            ts+=i;
+        }
+
+        return ts-sum;
+        
+    }
+}
